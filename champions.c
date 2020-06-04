ee3d8b0e3f6a8764d9918184dc70f74227597398
Regina-Astrid-Escalante-Nieves <65734579+Regina-Astrid-Escalante-Nieves@users.noreply.github.com>
2020-06-03 19:57:09 -0500

    Add files via upload

---
+++ champions.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+int a, b;
+while (a!=5){
+printf ("¿Qué es lo que deseas ver? \n");
+printf("1.Octavos\n");
+printf("2.Cuartos\n");
+printf("3.Semifinal \n");
+printf("4.Final \n");
+scanf("%d", &a);
+
+if (a==1){} 
+if (a==2){} 
+if (a==3){}
+if (a==4){}
+return 0;
+}
+
+
